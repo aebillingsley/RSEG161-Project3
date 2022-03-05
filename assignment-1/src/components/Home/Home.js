c046b7ebef4a15896134c9ad5151511a33044168
aebillingsley <63311203+aebillingsley@users.noreply.github.com>
2022-03-05 11:36:17 -0500

    Initial Directory Structure
    initial check in

---
+++ assignment-1/src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css';
+
+class Home extends React.Component 
+{
+    render()
+    {
+        return(
+            <div>
+                <h1>Hello!</h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
